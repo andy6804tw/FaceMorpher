2d64c1349cdc0326183302c3da3860c83b8d6e2b
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2021-05-06 15:12:14 +0800

    render result gif

--- morpher-app/src/containers/Home/component/Morph/Morph.js
+++ morpher-app/src/containers/Home/component/Morph/Morph.js
@@ -11,12 +11,13 @@ function Morph() {
   // if you don't care the onChange event, you can use a ref to retrieve the cropped file
   const fileRef_1 = useRef();
   const fileRef_2 = useRef();
+  let file_1,file_2;
   
   function handleChange_1(croppedFile) {
+    file_1=croppedFile;
     console.log(fileRef_1.current);
     let reader = new FileReader();
     reader.onload = function() {
-      console.log(document.getElementById('imgPreview_1').style)
       document.getElementById('imgPreview_1').style.backgroundImage = 'url(' + reader.result + ')';
       document.getElementById('imgPreview_1').setAttribute('data-text','');
       document.getElementById('imgStatus_1').setAttribute('data-text','edit');
@@ -26,10 +27,10 @@ function Morph() {
     reader.readAsDataURL(croppedFile);
   }
   function handleChange_2(croppedFile) {
+    file_2=croppedFile;
     console.log(fileRef_2.current);
     let reader = new FileReader();
     reader.onload = function() {
-      console.log(document.getElementById('imgPreview_2').style)
       document.getElementById('imgPreview_2').style.backgroundImage = 'url(' + reader.result + ')';
       document.getElementById('imgPreview_2').setAttribute('data-text','');
       document.getElementById('imgStatus_2').setAttribute('data-text','edit');
@@ -41,12 +42,12 @@ function Morph() {
   }
   
   async function call() {
-    console.log(fileRef_1.current);
-    console.log(fileRef_2.current);
-    const base64Image_1 = await toBase64(fileRef_1.current);
+    console.log(file_1);
+    console.log(file_2);
+    const base64Image_1 = await toBase64(file_1);
     // const sizeImage_1 = await getSize(fileRef_1.current);
     // const base64ImageCompress_1 = await getCompressImage(fileRef_1.current, sizeImage_1, base64Image_1);
-    const base64Image_2 = await toBase64(fileRef_2.current);
+    const base64Image_2 = await toBase64(file_2);
     // const sizeImage_2 = await getSize(fileRef_2.current);
     // const base64ImageCompress_2 = await getCompressImage(fileRef_2.current, sizeImage_2, base64Image_2);
 
@@ -60,7 +61,9 @@ function Morph() {
         // POST success
         const filename = dataObject.filename;
         const imgResult = dataObject.result.split("'")[1];
-        document.getElementById('imgResult').src = `data:image/jpg;base64,${imgResult}`;
+        console.log('done');
+        document.getElementById('showResult').classList.remove('d-none');
+        document.getElementById('imgResult').style.backgroundImage = `url(data:image/jpg;base64,${imgResult})`;
       },
         (error) => {
           // var message = error.response.data.message;
@@ -100,14 +103,14 @@ function Morph() {
           previewOptions={{ width: 254, height: 254 }}
         />
         {/* Resilt Image */}
-        <div className="box m-5">
+        <div className="box m-5 d-none" id="showResult">
           <div className="js--image-preview" data-text="" id="imgResult"></div>
           <div className="upload-options">
             <label className="label_result">Result</label>
           </div>
         </div>
       </div>
-      <button onClick={cal}>Click</button>
+      <button onClick={call}>Click</button>
       {/* <ImageCropper
         fileRef={fileRef_1}
         // onChange={handleChange}

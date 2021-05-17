ca17d9ab2e7accded4a63c0c9f5c4e46e62f00a0
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2021-05-17 14:53:18 +0800

    test

--- API/app/controllers/morph.py
+++ API/app/controllers/morph.py
@@ -4,27 +4,28 @@ from flask import Blueprint, request,jsonify,redirect
 from datetime import datetime
 import base64
 import config
-import app.modules.morph as morphModule
+# import app.modules.morph as morphModule
 
 morph = Blueprint('morph',__name__)
   
 
 @morph.route('', methods=['GET','POST'])
 def add():
-  if request.method == 'GET':
-    morphModule.image_to_video('')
-    return jsonify({'result': 'call GET from /morph'})
-  else:
-    insertValues = request.get_json()
-    image1=insertValues['image1']
-    image2=insertValues['image2']
-    filename='img_'+datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-    if config.DEBUG:
-      filename='test'
-    morphModule.image_to_video(insertValues, filename)
-    with open('app/static/'+filename+'.gif', "rb") as image_file:
-      encoded_string = base64.b64encode(image_file.read())
-      return jsonify({'filename':filename, 'result': str(encoded_string)})
+  # if request.method == 'GET':
+  #   morphModule.image_to_video('')
+  #   return jsonify({'result': 'call GET from /morph'})
+  # else:
+  #   insertValues = request.get_json()
+  #   image1=insertValues['image1']
+  #   image2=insertValues['image2']
+  #   filename='img_'+datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+  #   if config.DEBUG:
+  #     filename='test'
+  #   morphModule.image_to_video(insertValues, filename)
+  #   with open('app/static/'+filename+'.gif', "rb") as image_file:
+  #     encoded_string = base64.b64encode(image_file.read())
+  #     return jsonify({'filename':filename, 'result': str(encoded_string)})
+    return 'asdasd'
 
 @morph.route('/show')
 def show():
--- README.md
+++ README.md
@@ -1,7 +1,34 @@
-# 
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://novel-coronavirus-2019.herokuapp.com/)
+# FaceMorpher
 
+## API
+Deploy on Heroku. More detail you can see at API Doc.
 
-https://azmariewang.medium.com/
+### base64 圖片預測
+> POST https://face-morpher.herokuapp.com/morph 
 
-https://github.com/zhaoyao91/react-bootstrap-image-cropper
-https://codepen.io/Sphinxxxx/pen/pZQRGB
+- Body parameters
+
+|Body參數|類型|傳輸格式|描述|
+|:------:|:--:|:--:|:--:|
+|image1<font color=red>(必填)</font>|string|JSON|圖片base64編碼|
+|image2<font color=red>(必填)</font>|string|JSON|圖片base64編碼|
+
+```
+Request
+Content-Type: application/json
+
+{
+    "image1": "... base64 image",
+    "image2": "... base64 image",
+}
+```
+
+![](https://i.imgur.com/iODzXH6.png)
+
+## Reference
+[https://azmariewang.medium.com/](https://azmariewang.medium.com/)
+
+[https://github.com/zhaoyao91/react-bootstrap-image-cropper](https://github.com/zhaoyao91/react-bootstrap-image-cropper)
+
+[https://codepen.io/Sphinxxxx/pen/pZQRGB](https://codepen.io/Sphinxxxx/pen/pZQRGB)

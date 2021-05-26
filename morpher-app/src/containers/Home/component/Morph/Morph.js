d864949d67794a6097d30884bffdf850ace65f57
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2021-05-26 15:47:25 +0800

    add download func

--- morpher-app/src/containers/Home/component/Morph/Morph.js
+++ morpher-app/src/containers/Home/component/Morph/Morph.js
@@ -11,45 +11,46 @@ function Morph() {
   // if you don't care the onChange event, you can use a ref to retrieve the cropped file
   const fileRef_1 = useRef();
   const fileRef_2 = useRef();
-  let file_1,file_2;
-  
+  let file_1, file_2;
+
   function handleChange_1(croppedFile) {
-    file_1=croppedFile;
+    file_1 = croppedFile;
     console.log(fileRef_1.current);
     let reader = new FileReader();
-    reader.onload = function() {
+    reader.onload = function () {
       document.getElementById('imgPreview_1').style.backgroundImage = 'url(' + reader.result + ')';
-      document.getElementById('imgPreview_1').setAttribute('data-text','');
-      document.getElementById('imgStatus_1').setAttribute('data-text','edit');
+      document.getElementById('imgPreview_1').setAttribute('data-text', '');
+      document.getElementById('imgStatus_1').setAttribute('data-text', 'edit');
       // document.getElementById('imgPreview_1').style.setProperty('--content','');
       // document.getElementById('imgPreview_1').sheet.insertRule("#imgPreview_1::before{color:blue;}", 0);
     }
     reader.readAsDataURL(croppedFile);
     checkUploadDone();
   }
-  function cal(){
+  function cal() {
     document.getElementById('imgResult').style.backgroundImage = `url('https://i.imgur.com/u88PcD5.gif')`;
   }
   function handleChange_2(croppedFile) {
-    file_2=croppedFile;
+    file_2 = croppedFile;
     console.log(fileRef_2.current);
     let reader = new FileReader();
-    reader.onload = function() {
+    reader.onload = function () {
       document.getElementById('imgPreview_2').style.backgroundImage = 'url(' + reader.result + ')';
-      document.getElementById('imgPreview_2').setAttribute('data-text','');
-      document.getElementById('imgStatus_2').setAttribute('data-text','edit');
+      document.getElementById('imgPreview_2').setAttribute('data-text', '');
+      document.getElementById('imgStatus_2').setAttribute('data-text', 'edit');
     }
     reader.readAsDataURL(croppedFile);
     checkUploadDone();
   }
-  function checkUploadDone(){
-    if(file_1&&file_2){
+  function checkUploadDone() {
+    if (file_1 && file_2) {
       document.getElementById('morphButton').classList.remove('d-none');
     }
   }
-  function refreshPage(){
+  function refreshPage() {
     window.location.reload();
-} 
+  }
+  let imgResult;
   async function handleMorph() {
     document.getElementById('loading').classList.remove('d-none');
     console.log(file_1);
@@ -70,10 +71,10 @@ function Morph() {
         var dataObject = response.data;
         // POST success
         const filename = dataObject.filename;
-        const imgResult = dataObject.result.split("'")[1];
-        console.log('done',filename);
+        imgResult = dataObject.result.split("'")[1];
+        console.log('done', filename);
         document.getElementById('showResult').classList.remove('d-none');
-        document.getElementById('imgResult').style.backgroundImage = `url(data:image/jpg;base64,${imgResult})`;
+        document.getElementById('imgResult').style.backgroundImage = `url(data:image/gif;base64,${imgResult})`;
         document.getElementById('morphButton').classList.add('d-none');
         document.getElementById('replayButton').classList.remove('d-none');
         document.getElementById('loading').classList.add('d-none');
@@ -83,6 +84,15 @@ function Morph() {
           console.log('QQ失敗了 ' + error);
         }
       );
+  }
+  // download image
+  function downloadImg() {
+    let a = document.createElement('a');
+    a.href = `data:image/gif;base64,${imgResult}`;
+    a.download = "output.gif";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
   }
   return (
     <div>
@@ -116,7 +126,7 @@ function Morph() {
           previewOptions={{ width: 254, height: 254 }}
         />
         {/* Resilt Image */}
-        <div className="box m-5 d-none" id="showResult">
+        <div className="box m-5 d-none" id="showResult" onClick={downloadImg}>
           <div className="js--image-preview" data-text="" id="imgResult"></div>
           <div className="upload-options">
             <label className="label_result" id="labelResult" data-text="download"></label>
@@ -124,10 +134,10 @@ function Morph() {
         </div>
       </div>
       <div className="text-center">
-      <button className="button-morph animate__animated animate__heartBeat d-none" id="morphButton" data-text="" onClick={handleMorph}> {'\u00A0\u00A0'}Start{'\u00A0\u00A0'} </button>
-      <button className="button-morph animate__animated animate__heartBeat d-none" id="replayButton" data-text="replay" onClick={refreshPage}> {'\u00A0\u00A0'}Try Again{'\u00A0\u00A0'} </button>
-      {/* <button type="button" class="btn btn-warning" onClick={call}>Click</button> */}
-        </div>
+        <button className="button-morph animate__animated animate__heartBeat d-none" id="morphButton" data-text="" onClick={handleMorph}> {'\u00A0\u00A0'}Start{'\u00A0\u00A0'} </button>
+        <button className="button-morph animate__animated animate__heartBeat d-none" id="replayButton" data-text="replay" onClick={refreshPage}> {'\u00A0\u00A0'}Try Again{'\u00A0\u00A0'} </button>
+        {/* <button type="button" class="btn btn-warning" onClick={call}>Click</button> */}
+      </div>
       {/* <ImageCropper
         fileRef={fileRef_1}
         // onChange={handleChange}
